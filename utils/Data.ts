6d0e0325394be2f1ca5afce357867ca87aaf0b58
ikram-maulana <ikram075@ummi.ac.id>
2022-09-20 17:51:00 +0700

    Fix(Error): Fix Error in Production Because API

--- utils/Data.ts
+++ utils/Data.ts
@@ -1,11 +1,15 @@
 export const gradesAverage = async () => {
-  const res = await fetch("http://localhost:3000/api/gradesAverage");
+  const res = await fetch(
+    "https://6329979bd2c97d8c526b0aca.mockapi.io/api/gradesAverage"
+  );
   const data = await res.json();
   return data;
-}
+};
 
 export const gradesRule = async () => {
-  const res = await fetch("http://localhost:3000/api/gradesRule");
+  const res = await fetch(
+    "https://6329979bd2c97d8c526b0aca.mockapi.io/api/gradesRule"
+  );
   const data = await res.json();
   return data;
-}
+};
